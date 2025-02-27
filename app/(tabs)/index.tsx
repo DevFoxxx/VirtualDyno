1de0149dc58be0540613ba3aed1618d1be958f12
Tornike <tornike_kutaisi@yahoo.ca>
2025-02-27 16:21:30 -0500

    modified language selector to not show the text of the current language

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -207,10 +207,11 @@ export default function HomeScreen() {
           paddingHorizontal: 30,
         }}
       >
+        {/*Language selector, current language text is not displayed  */}
         <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <Text style={dynamicStyles.languageText}>IT</Text>
+          <Text style={[dynamicStyles.languageText]}>{isEnglish && 'IT'}</Text>
           <Switch value={isEnglish} onValueChange={handleLanguageToggle} />
-          <Text style={dynamicStyles.languageText}>EN</Text>
+          <Text style={[dynamicStyles.languageText]}>{!isEnglish && 'EN'}</Text>
         </View>
 
         {/* Day/night mode switch using expo icon //////////////////////////////*/}
